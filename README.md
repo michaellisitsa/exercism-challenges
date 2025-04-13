1fcfe483f30ec9564f80c29961feedb83a0c3223
Michael Lisitsa <michael.lisitsa@gmail.com>
2025-04-13 14:20:01 +1000

    Add first few python challenges - Hello world - Simple Linked Lists (first 5 tests passing)

---
+++ README.md
@@ -0,0 +1,9 @@
+# Exercism.org Challenges
+
+Follow my journey here:
+https://exercism.org/profiles/mlisitsa
+
+## Languages
+
+- C++ https://exercism.org/tracks/cpp
+- Python https://exercism.org/tracks/python
---
+++ python/simple-linked-list/simple_linked_list.py
@@ -0,0 +1,105 @@
+from __future__ import annotations
+import typing
+
+
+class EmptyListException(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class Node:
+    _next: typing.Optional[Node]
+    _value: int
+
+    def __init__(self, value: int):
+        """
+        Each song is a number
+
+        """
+        # Store the value
+        self._value = value
+        pass
+
+    def value(self) -> int:
+        return self._value
+
+    def next(self) -> typing.Optional[Node]:
+        try:
+            return self._next
+        except:
+            return None
+
+
+class LinkedList:
+    _head: Node
+
+    def __init__(self, values=None):
+        if values is None:
+            return
+        previousNode = None
+        for value in values:
+            node = Node(value)
+            print("calling node ", node, " with value ", value)
+            if previousNode:
+                print("previous node ", previousNode)
+                previousNode._next = node
+            else:
+                # We are on the first iteration.
+                # Let's save this to head
+                self._head = node
+            previousNode = node
+
+    def __iter__(self):
+        """
+        Supports looping through the list
+        """
+        pass
+
+    def __len__(self):
+        """
+        We're calling len() in the tests to determine length of list
+        """
+        length = 0
+        try:
+            currentNode: typing.Optional[Node] = self.head()
+            length += 1
+        except EmptyListException:
+            # Do not throw an exception on accessing head.
+            # We want to still have this query accessible
+            # as the LinkedList may be empty to start but filled with push
+            return length
+
+        while currentNode and currentNode.next():
+            # Checking currentNode
+            print("currentNode", currentNode)
+            length += 1
+            currentNode = currentNode.next()
+        return length
+
+    def head(self) -> typing.Optional[Node]:
+        try:
+            return self._head
+        except:
+            raise EmptyListException("The list is empty.")
+
+    def push(self, value):
+        """
+        Push to the head of the linked list
+        Much easier than pushing to the back because you don't need
+        to navigate O(n) to the end.
+        """
+        newHead = Node(value)
+        newHead._next = self.head()
+
+    def pop(self):
+        pass
+
+    def reversed(self):
+        pass
+
+
+# Given a range of numbers
+list = LinkedList([1, 2, 3, 4, 5])
+len(list)
+# Expect a linked list for each number
+#
