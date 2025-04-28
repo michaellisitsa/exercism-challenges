a78b619e57e50090968ec4d45059f0487111b58d
Michael Lisitsa <michael@lisitsa.dev>
2025-04-28 22:06:44 +1000

    Add get handler using new User dataclasses - post handler is now broken until we update it with   the new dataclasses as well

--- python/rest-api/rest_api.py
+++ python/rest-api/rest_api.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import json
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 from typing import TypedDict
 
 
@@ -37,20 +37,13 @@ class RestAPI:
 
     # TODO: how to type a dict with specific key "users" and list of dict
     def __init__(self, database: DatabaseDict | None = None):
-        """
-        Ingest database with users like below
-        "users": [
-            {"name": "Adam", "owes": { "Bob": 12.0, "Chuck": 4.0 }, "owed_by": { "Bob": 6.5, "Dan": 2.75 }, "balance": 0.0},
-        ]
-        """
-        # TODO: Do I need to normalize the data, or just have getters for different things like amount
         self._data = Database()
         if database:
             for user in database["users"]:
                 self.set_user(user)
 
     def set_user(self, data):
-        # Only name is required by the User class
+        # Only name is requred by the User class
         user = User(
             data["name"],
             data["owes"],
@@ -61,6 +54,9 @@ class RestAPI:
 
     def get_user(self, name):
         return self._data[name]
+
+    def get_users(self):
+        return [asdict(user) for user in self._data.values()]
 
     def fetchData(self, userId) -> dict | None:
         # TODO: Get a better find function
@@ -84,16 +80,14 @@ class RestAPI:
             return user
 
     def get(self, url, payload=None):
-        """
-        payload = {"users":["Adam","Bob"]}
-        """
-        if payload is None:
-            return json.dumps(self._data)
-        # TODO: Add a decorator to decode and encode. This is is getting old
-        payload_decoded = json.loads(payload)
         if url.endswith("users"):
-            filtered_users = map(self.fetchData, payload_decoded["users"])
-            return json.dumps({"users": list(filtered_users)})
+            if payload is None:
+                return json.dumps({"users": self.get_users()})
+            else:
+                users = [
+                    asdict(self.get_user(user)) for user in json.loads(payload)["users"]
+                ]
+                return json.dumps({"users": users})
 
     def post(self, url, payload=None):
         if payload is None:
