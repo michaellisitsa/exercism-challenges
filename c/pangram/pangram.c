85e1ef72585aba70a8b8e433a3da4587394d79a0
Michael Lisitsa <michael@lisitsa.dev>
2025-11-11 21:31:53 +1100

    C Exercism examples

---
+++ c/pangram/pangram.c
@@ -0,0 +1,41 @@
+#include "pangram.h"
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+bool is_pangram(const char *sentence) {
+  // int count = sizeof(*sentence) / sizeof(sentence[0]);
+  if (sentence == NULL) {
+    return false;
+  } else if (sentence[0] == '\0') {
+    // This statement isn't even needed as you can just loop over the zero
+    // length item
+    return false;
+  }
+  // Create an array of 26 length
+  // Fill the array with zeros
+  int charTracker[26] = {0};
+  // For each letter, get the item at the array slot and change to 1.
+  for (uint32_t i = 0; i < strlen(sentence); i++) {
+    int asciiValue;
+    asciiValue = sentence[i];
+
+    // Stdlib alsa has an isalpha, and a tolower
+    if (asciiValue >= 97 && asciiValue <= 122) {
+      charTracker[asciiValue - 97] = 1;
+    } else if (asciiValue >= 65 && asciiValue <= 90) {
+      charTracker[asciiValue - 65] = 1;
+    }
+  }
+  int count = 0;
+  // Possible optimisation: As soon as we encounter a zero, we can return false.
+  // see https://exercism.org/tracks/c/exercises/pangram/solutions/archanarawat
+  for (int i = 0; i < 26; i++) {
+    // Iterate over the array counting to see if adds up to 26.
+    count += charTracker[i];
+  }
+  if (count == 26)
+    return true;
+  return false;
+}
---
+++ c/resistor-color-duo/resistor_color_duo.h
@@ -0,0 +1,22 @@
+#ifndef RESISTOR_COLOR_DUO_H
+#define RESISTOR_COLOR_DUO_H
+
+// Extended int types
+#include <stdint.h>
+
+typedef enum {
+  BLACK = 0,
+  BROWN,
+  RED,
+  ORANGE,
+  YELLOW,
+  GREEN,
+  BLUE,
+  VIOLET,
+  GREY,
+  WHITE,
+} resistor_band_t;
+
+uint16_t color_code(resistor_band_t colors[2]);
+
+#endif
